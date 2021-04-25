7a781381735d8e4cdb34a35bf513a7b8070c00b3
lauraariza-alt <75591063+lauraariza-alt@users.noreply.github.com>
2021-04-25 18:20:46 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Activación de transición del menú principal
+
+  
+$('nav#menu a, aside#menu-responsive nav a').bind('click',function(event){
+    var $anchor = $(this);  
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1000,'easeOutExpo');
+    event.preventDefault();
+});
+
+// Función Abrir- Cerrar del menú mobile
+
+$("#abrir").click(function(){
+  $("#menu-responsive").animate({
+      right:0
+  });
+
+  $("#abrir").hide();
+  $("#cerrar").show();
+  event.preventDefault();
+
+});
+
+$("#cerrar").click(function( ){
+    $("#menu-responsive").animate({
+      right:-290
+});
+
+$("#cerrar").hide();
+$("#abrir").show();
+event.preventDefault();
+
+});
+
+ 
